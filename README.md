7faa7c86523ae2726739a6ffb7a3752466021c3c
Ivan Orekhov <i.a.orekhov@gmail.com>
2026-02-06 17:20:21 +0300

    initial commit: maven java project (jira rest client)

---
+++ README.md
@@ -0,0 +1,1 @@
+# jira-pet-java
---
+++ src/test/java/io/github/iaorekhov/jiraclient/AppTest.java
@@ -0,0 +1,16 @@
+package io.github.iaorekhov.jiraclient;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test 
+    void basic() { 
+        assertTrue(true);         
+    }
+}
+
+
